b3a271b02c1d9483890202229de98377b4273676
t1rs <147614066+t1rs@users.noreply.github.com>
2023-11-05 15:59:56 +0500

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,10 +1,24 @@
-import Heron_square
-
 def main():
-    a = float(input("Введите длину стороны a: "))
-    b = float(input("Введите длину стороны b: "))
-    c = float(input("Введите длину стороны c: "))
-    print("Площадь треугольника равна: ", Heron_square.SquareHeron(a, b, c))
+    list_1 = [1, 1, 3, 3, 1]
+    list_1.sort()
+    list = []
+    number = list_1[0]
+    str1 = ""
+    for item in list_1:
+        if number == item:
+            number = item
+            str1 += str(item)
+            list.append(str1)
+        else:
+            number = item
+            str1 = ''
+            str1 += str(item)
+            list.append(str1)
+    for item in list:
+        if len(item) == 1:
+            list.remove(item)
+            list.insert(0, int(item))
+    print("Вывод программы -", list)
 
 if __name__ == "__main__":
     main()

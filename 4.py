8a6566b28fb7c07890a76dd7fbab96fb5ae39cd0
t1rs <147614066+t1rs@users.noreply.github.com>
2023-11-05 15:58:43 +0500

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,12 @@
-import statistics
-
-def main(*args):
-    print(statistics.mean(args))
+def main():
+    my_list = [5, 4, 3, 3, 4, 3, 3, 5, 5, 3, 3, 3, 3, 4, 4]
+    my_list1 = []
+    for item in my_list:
+        if item != 2:
+            if item == 3:
+                item = 4
+            my_list1.append(item)
+    print(my_list1)
 
 if __name__ == "__main__":
-    main(1, 2, 3, 4)
+    main()

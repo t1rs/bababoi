dcb5488ebffda13c8ebe206ff461fddbea61a1a9
t1rs <147614066+t1rs@users.noreply.github.com>
2023-11-05 15:17:19 +0500

    Delete Сам_раб directory

--- Сам_раб/5.2.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def calculate_area(side_a, side_b, side_c):
-    # Расчет полупериметра
-    s = (side_a + side_b + side_c) / 2
-    # Вычисление площади треугольника по формуле Герона
-    area = math.sqrt(s * (s - side_a) * (s - side_b) * (s - side_c))
-    return area

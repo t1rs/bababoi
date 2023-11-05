b991a584e4cec93d8e072c12468b017d4717ad9a
t1rs <147614066+t1rs@users.noreply.github.com>
2023-11-05 15:57:42 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,13 +1,30 @@
-import Heron
+import math
 
-def main():
-    one = [12, 25, 3, 48, 71]
-    two = [5, 18, 40, 62, 98]
-    three = [4, 21, 37, 56, 84]
-    my_conList = one+two+three
-    my_conList.sort()
-    print("площади треугольника, составленного из минимальных элементов -", Heron.SquareHeron(my_conList[0], my_conList[1], my_conList[2]))
-    print("площади треугольника, составленного из максимальных элементов -", Heron.SquareHeron(my_conList[len(my_conList)-1], my_conList[len(my_conList)-2], my_conList[len(my_conList)-3]))
+def calculate_area(sides):
+    a, b, c = sides
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    return area
 
-if __name__ == "__main__":
-    main()
+def calculate_triangle_areas(one, two, three):
+    min_one = min(one)
+    max_one = max(one)
+
+    min_two = min(two)
+    max_two = max(two)
+
+    min_three = min(three)
+    max_three = max(three)
+
+    area1 = calculate_area([min_one, min_two, min_three])
+    area2 = calculate_area([max_one, max_two, max_three])
+
+    return area1, area2
+one = [12, 25, 3, 48, 71]
+two = [5, 18, 40, 62, 98]
+three = [4, 21, 37, 56, 84]
+
+area1, area2 = calculate_triangle_areas(one, two, three)
+
+print("Площадь треугольника, составленного из минимальных элементов:", area1)
+print("Площадь треугольника, составленного из максимальных элементов:", area2)

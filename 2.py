c0fe7d1c5038c64ac6169f2377cc005fa313fe0d
t1rs <147614066+t1rs@users.noreply.github.com>
2024-03-22 14:21:57 +0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,6 +3,6 @@ def fib(n):
     with open('fib.txt', 'w') as file:
          for _ in range(n):
              file.write(str(a) + '\n')
-             a, b = b, a + b
              yield a
+             a, b = b, a + b
 print(list(fib(200))[199])
